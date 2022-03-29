2f49a8685b60c9f4382351a957ef271e49bb6492
Carlos Urquiza <webquiza@gmail.com>
2022-03-28 20:48:48 -0700

    add third passing test

--- index.test.js
+++ index.test.js
@@ -12,6 +12,11 @@ describe("fizzBuzz()", () => {
     expect(fizzBuzz(33)).toBe("Fizz");
   });
 
+  it('returns "Buzz" for multiples of 5', () => {
+    expect(fizzBuzz(5)).toBe("Buzz");
+    expect(fizzBuzz(20)).toBe("Buzz");
+  });
+
   it("returns the given number for multiples of neither 3 nor 5", () => {
     expect(fizzBuzz(1)).toBe("1");
     expect(fizzBuzz(22)).toBe("22");
